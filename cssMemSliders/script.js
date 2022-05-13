5375d50f6ecb361b2a0d6be1b3836dead15a846d
olvsivkov <1000listov@mail.ru>
2022-05-14 01:14:29 +0300

    feat: add animation to text content(sat, may 14, 2022 01.11 am)

--- cssMemSliders/script.js
+++ cssMemSliders/script.js
@@ -3,15 +3,35 @@ let dots = document.getElementsByClassName("slider-kontroller");
 let text = document.querySelector(".mem-text");
 text.textContent = "crazy woman"
 
+function opacityStyle() {
+    text.style.opacity = "0.5";
+    setTimeout(() => text.style.opacity = "0.6", 100);
+    setTimeout(() => text.style.opacity = "0.7", 200);
+    setTimeout(() => text.style.opacity = "0.8", 300);
+    setTimeout(() => text.style.opacity = "1", 500);
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
-    if (n === 1) text.textContent = "crazy woman"
-    if (n === 2) text.textContent = "the cat screams at the girls"
-    if (n === 3) text.textContent = "This is fine"
-    if (n === 4) text.textContent = "when you shifted the div by 1 pixel in your website"
+    if (n === 1) {
+        text.textContent = "crazy woman"
+        opacityStyle()
+    }
+    if (n === 2) {
+        text.textContent = "the cat screams at the girls"
+        opacityStyle()
+    }
+    if (n === 3) {
+        text.textContent = "This is fine"
+        opacityStyle()
+    }
+    if (n === 4) {
+        text.textContent = "when you shifted the div by 1 pixel in your website"
+        opacityStyle()
+    }
 }
 
 function showSlides(n) {
